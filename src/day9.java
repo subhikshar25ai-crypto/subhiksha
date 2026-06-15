46c8406756e9638fdce63567af761e6f92174b9d
subhikshar25ai-crypto <subhikshar25ai@vetias.ac.in>
2026-06-15 10:28:58 +0530

    day9

--- src/day9.java
+++ src/day9.java
@@ -1,2 +1,45 @@
-public class day9 {
+public class Day9 {
+
+    // Login
+    sealed interface LoginResult
+            permits LoginSuccess, LoginFailure, LoginLoading {
+    }
+
+    record LoginSuccess(String username) implements LoginResult {
+    }
+
+    record LoginFailure(String reason) implements LoginResult {
+    }
+
+    record LoginLoading(String message) implements LoginResult {
+    }
+
+    static void handleLogin(LoginResult result) {
+        switch (result) {
+            case LoginSuccess success ->
+                    System.out.println("Login Successful : " + success.username());
+
+            case LoginFailure failure ->
+                    System.out.println("Login Failed because of " + failure.reason());
+
+            case LoginLoading loading ->
+                    System.out.println("Login Loading... " + loading.message());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        LoginResult loginSuccess =
+                new LoginSuccess("John");
+
+        LoginResult loginFailure =
+                new LoginFailure("Invalid Password");
+
+        LoginResult loginLoading =
+                new LoginLoading("Please wait...");
+
+        handleLogin(loginSuccess);
+        handleLogin(loginFailure);
+        handleLogin(loginLoading);
+    }
 }
