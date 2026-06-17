ddda2db0f22448178011b9b8c437293cbc8a9395
Subhiksha <subhikshar25ai@vetias.ac.in>
2026-06-17 16:15:31 +0530

    exception

--- src/day11.java
+++ src/day11.java
@@ -4,14 +4,14 @@ import java.util.Map;
 public class day11{
     public static void main(String[] args) {
         Map<String, Object> Students = new HashMap<>();
-        Students.put("Ramesh":99);
-        Students.put("dharani": 90);
-        Students.put("vasanth":88);
-        Students.put("venkat":70);
+        Students.put("Ramesh",99);
+        Students.put("dharani",90);
+        Students.put("vasanth",88);
+        Students.put("venkat",70);
 
         System.out.println(Students.get("Ramesh"));
         Students.entrySet().forEach(entry ->
-            System.out.println(entry.getKey() + " : " + entry.getValue());
+            System.out.println(entry.getKey() + " : " + entry.getValue()));
         }
     }
 
